0d8b8f7af0676b22d9e4272fbd86061bd9c8b2e6
arvynncharlesyap <53499727+arvynncharlesyap@users.noreply.github.com>
2019-07-31 09:45:12 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,22 @@
 <html>
 <head>
 <title> Test </title>
-  <body>
-  <h1> Hello!</h1>
-  </body>
-  </head>
-  </html>
+<article>
+<header>
+<h1><p> This is a Header tag. </p></h1>
+</header>
+<p>The header element represents a container for introductory content or a set of navigational links. A <header> element typically contains: one or more heading elements (h1 - h6) logo or icon.</p>
+</article>
+
+<main>
+  <h1>Main tag</h1>
+  <p>The main tag specifies the main content of a document.</p>
+  </main>
+  
+  <footer>
+  <h1> This is a footer </h1>
+  <p>Made by: Arvynn Charles Yap</p>
+  <p>Contact information: <a href="arvynncharlesyap2018.gmail.com">arvynncharlesyap2018@gmail.com</a>.</p>
+</footer>
+</head>
+</html>
