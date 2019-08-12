dcf1c0d1b9d48a875853f1374714fa5957a646fa
arvynncharlesyap <53499727+arvynncharlesyap@users.noreply.github.com>
2019-08-12 08:48:13 +0800

    Create firstphoto.php

---
+++ firstphoto.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title> First Page </title>
+</head>
+<body>
+<body bgcolor="lightblue">
+<center>
+<h1> YapA Photo Gallery </h1>
+<img src="firstphoto.jpg"><br>
+<br>
+<br>
+<a href="homepage.php">Go back to Home</a>
+<a href="secondphoto.php">Next Photo</a>
