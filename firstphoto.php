71523fc47184addf7d7dcd465b8ac96af83af9bc
arvynncharlesyap <53499727+arvynncharlesyap@users.noreply.github.com>
2019-08-12 08:56:15 +0800

    Update firstphoto.php

--- firstphoto.php
+++ firstphoto.php
@@ -9,6 +9,5 @@
 <h1> YapA Photo Gallery </h1>
 <img src="firstphoto.jpg" width="500" height="500"> <br>
 <br>
-<br>
 <a href="homepage.php">Go back to Home</a>
 <a href="secondphoto.php">Next Photo</a>
