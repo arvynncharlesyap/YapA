4b4507996ddcb2fedb53d609c771120501b19bcd
arvynncharlesyap <53499727+arvynncharlesyap@users.noreply.github.com>
2019-08-12 08:38:50 +0800

    Rename index.php to homepage.php

---
+++ homepage.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<html lang="en">
+<head>
+<title> Home </title>
+</head>
+<body>
+<body bgcolor="lightblue">
+<center>
+<h1> YapA Photo Gallery </h1>
+<h3> List of Photos </h3>
+<a href="firstphoto.jpg">First Picture</a><br>
+<a href="secondphoto.jpg">Second Picture</a><br>
+<a href="thirdphoto.jpg">Third Picture</a><br>
+
+</body>
+</html>
