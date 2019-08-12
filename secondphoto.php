6f42911f943708862935d35d64a2b4eab2dd3901
arvynncharlesyap <53499727+arvynncharlesyap@users.noreply.github.com>
2019-08-12 09:03:30 +0800

    Create secondphoto.php

---
+++ secondphoto.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title> First Page </title>
+</head>
+<body>
+<body bgcolor="lightblue">
+<center>
+<h1> YapA Photo Gallery </h1>
+<img src="secondphoto.jpg" width="500" height="500"> <br>
+<br>
+<a href="index.php">Go back to Home</a>
+<a href="thirdphoto.php">Next Photo</a>
