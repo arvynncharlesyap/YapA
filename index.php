7466dfd02f1fc79bb9d4342443f3ff4578804782
arvynncharlesyap <53499727+arvynncharlesyap@users.noreply.github.com>
2019-08-12 09:01:24 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,9 +9,9 @@
 <center>
 <h1> YapA Photo Gallery </h1>
 <h3> List of Photos </h3>
-<a href="firstphoto.jpg">First Picture</a><br>
-<a href="secondphoto.jpg">Second Picture</a><br>
-<a href="thirdphoto.jpg">Third Picture</a><br>
+<a href="firstphoto.php">First Picture</a><br>
+<a href="secondphoto.php">Second Picture</a><br>
+<a href="thirdphoto.php">Third Picture</a><br>
 
 </body>
 </html>
