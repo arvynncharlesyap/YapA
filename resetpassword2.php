6ebcfa65d2c9ca697496684a4489886091b3690a
arvynncharlesyap <53499727+arvynncharlesyap@users.noreply.github.com>
2019-08-14 14:44:33 +0800

    Create resetpassword2.php

---
+++ resetpassword2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Success!</title>
+</head>
+<body align="center">
+<body bgcolor="lightblue">
+<h1> Success! </h1>
+<h1> Kindly check your e-mail account. </h1>
+<p> Go back to
+<a href="index.php">login page.</a>
+
+</body>
+</html>
