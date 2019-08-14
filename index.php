af089c18984f0ee8fec5d50c8b69ca91fa2ba442
arvynncharlesyap <53499727+arvynncharlesyap@users.noreply.github.com>
2019-08-14 09:46:07 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login</title>
+</head>
+<body align="center">
+<h1>Login</h1>
+</br>
+<form submit="/submit_page.php">
+<h4>Username:<input type="username" placeholder="Input Username"></h4>
+<h4>Password:<input type="password" placeholder="Input Password"</h4>
+</br>
+</br>
+<input type="submit" value="Sign In">
+</form>
+</br>
+<a href="resetpassword.php">Reset Password</a>
+&nbsp;&nbsp;
+<a href="signup.php">Sign Up</a>
