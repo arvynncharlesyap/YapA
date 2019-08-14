7ef082e6671ff7f97bdb32491cc5c1eb28cb7097
arvynncharlesyap <53499727+arvynncharlesyap@users.noreply.github.com>
2019-08-14 14:59:10 +0800

    Update thirdphoto.php

--- thirdphoto.php
+++ thirdphoto.php
@@ -3,10 +3,11 @@
 <head>
 <title> Third Page </title>
 </head>
-<body>
 <body bgcolor="lightblue">
 <center>
 <h1> YapA Photo Gallery </h1>
 <img src="thirdphoto.jpg" width="500" height="500"> <br>
 <br>
 <a href="index2.php">Go back to Home</a>
+  </body>
+  </html>
