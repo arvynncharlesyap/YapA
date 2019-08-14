2d590f75b94cc8e6d09a10121debffe967cab154
arvynncharlesyap <53499727+arvynncharlesyap@users.noreply.github.com>
2019-08-14 15:02:18 +0800

    Create signup2.php

---
+++ signup2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Success!</title>
+</head>
+<body align="center">
+<body bgcolor="lightblue">
+<h1> Success! </h1>
+<h1> You may now login your account.</h1>
+<p> Go back to
+<a href="index.php">login page.</a>
+</body>
+</html>
