f4008cdea97ea720d61487d4ff6c1a648057cbb0
arvynncharlesyap <53499727+arvynncharlesyap@users.noreply.github.com>
2019-08-14 14:39:05 +0800

    Update resetpassword.php

--- resetpassword.php
+++ resetpassword.php
@@ -10,8 +10,10 @@
 </br>
 </br>
 </br>
+<form action="index.php">
 <h4> Please enter your Email Address:
 <input type="text" placeholder="Input your e-mail address." </h4>
 <input type="submit" value"Reset Password>
+</form>
 </body>
 </html>
