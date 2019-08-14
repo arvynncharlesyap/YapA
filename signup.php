0e645828ae7b61893027bc33961cf510d2946b96
arvynncharlesyap <53499727+arvynncharlesyap@users.noreply.github.com>
2019-08-14 10:03:25 +0800

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Sign Up</title>
+</head>
+<body align="left">
+<h2>Sign Up</h3>
+</align>
+<center>
+<h4>Email Address:<input type="text"></h4>
+<h4>Confirm Email:<input type="text"></h4>
+<h4>Password:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text"></h4>
+<h4>Confirm Password:<input type="text"></h4>
+<input type="submit" value="Sign up">
+</body>
+</html>
