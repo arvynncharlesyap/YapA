25936819c68a32d46ba922e8a562db6416bf06e4
arvynncharlesyap <53499727+arvynncharlesyap@users.noreply.github.com>
2019-10-01 16:28:55 -0700

    Update game.php

--- game.php
+++ game.php
@@ -1,19 +1,16 @@
+
 <!DOCTYPE html>
 <html>
 <head>
 <title>Dr. Chuck's Rock, Paper, Scissors Game</title>
-<?php require_once "bootstrap.php"; ?>
+<link rel="stylesheet" href="game.css">
 </head>
 <body>
 <div class="container">
 <h1>Rock Paper Scissors</h1>
-<?php
-if ( isset($_REQUEST['name']) ) {
-    echo "<p>Welcome: ";
-    echo htmlentities($_REQUEST['name']);
-    echo "</p>\n";
-}
-?>
+
+<p class="one"><label><b>Click "Try It" to enter your Name :</b></label>
+<button onclick="myFunction()">Try it</button>
 <form method="post">
 <select name="human">
 <option value="-1">Select</option>
@@ -26,56 +23,26 @@ if ( isset($_REQUEST['name']) ) {
 <input type="submit" name="logout" value="Logout">
 </form>
 
+
+
 <pre>
-<?php
-if ( $human == -1 ) {
-    print "Please select a strategy and press Play.\n";
-} else if ( $human == 3 ) {
-    for($c=0;$c<3;$c++) {
-        for($h=0;$h<3;$h++) {
-            $r = check($c, $h);
-            print "Human=$names[$h] Computer=$names[$c] Result=$r\n";
-        }
-    }
-} else {
-    print "Your Play=$names[$human] Computer Play=$names[$computer] Result=$result\n";
-}
-?>
+Please select a strategy and press Play.
 </pre>
 </div>
-    // Demand a GET parameter
-if ( ! isset($_GET['name']) || strlen($_GET['name']) < 1  ) {
-    die('Name parameter missing');
-}
-// If the user requested logout go back to index.php
-if ( isset($_POST['logout']) ) {
-    header('Location: index.php');
-    return;
-}
-// Set up the values for the game...
-// 0 is Rock, 1 is Paper, and 2 is Scissors
-$names = array('Rock', 'Paper', 'Scissors');
-$human = isset($_POST["human"]) ? $_POST['human']+0 : -1;
-$computer = 0; // Hard code the computer to rock
-// TODO: Make the computer be random
-// $computer = rand(0,2);
-// This function takes as its input the computer and human play
-// and returns "Tie", "You Lose", "You Win" depending on play
-// where "You" is the human being addressed by the computer
-function check($computer, $human) {
-    // For now this is a rock-savant checking function
-    // TODO: Fix this
-    if ( $human == 0 ) {
-        return "Tie";
-    } else if ( $human == 1 ) {
-        return "You Win";
-    } else if ( $human == 2 ) {
-        return "You Lose";
-    }
+
+<script>
+function myFunction() {
+  var person = prompt("Please enter your name", "");
+  if (person != null) {
+    document.getElementById("demo").innerHTML =
+    "Welcome. " + person;
+  }
+
+   if ( person == "" ) {
+    alert("Name parameter missing.");
     return false;
+    }
 }
-// Check to see how the play happenned
-$result = check($computer, $human);
-?>
+</script>
 </body>
 </html>
