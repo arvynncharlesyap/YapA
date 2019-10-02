1a45424cc384657ee38294d19d78e5b106b2231f
arvynncharlesyap <53499727+arvynncharlesyap@users.noreply.github.com>
2019-10-02 08:54:51 +0800

    Update game.php

--- game.php
+++ game.php
@@ -1,16 +1,44 @@
+<?php
 
+if ( isset($_POST['logout']) ) {
+    header('Location: index.php');
+    return;
+}
+
+
+$names = array('Rock', 'Paper', 'Scissors');
+$human = isset($_POST["human"]) ? $_POST['human']+0 : -1;
+
+$computer = 0;
+
+function check($computer, $human) {
+  
+    if ( $human == 0 ) {
+        return "Tie";
+    } else if ( $human == 1 ) {
+        return "You Win";
+    } else if ( $human == 2 ) {
+        return "You Lose";
+    }
+    return false;
+}
+
+
+$result = check($computer, $human);
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
 <title>Dr. Chuck's Rock, Paper, Scissors Game</title>
-<link rel="stylesheet" href="game.css">
+<?php require_once "bootstrap.php"; ?>
 </head>
 <body>
 <div class="container">
 <h1>Rock Paper Scissors</h1>
+<?php
 
-<p class="one"><label><b>Click "Try It" to enter your Name :</b></label>
-<button onclick="myFunction()">Try it</button>
+
 <form method="post">
 <select name="human">
 <option value="-1">Select</option>
@@ -20,29 +48,25 @@
 <option value="3">Test</option>
 </select>
 <input type="submit" value="Play">
-<a href="index.php">Logout</a>
+<input type="submit" name="logout" value="Logout">
 </form>
 
-
-
 <pre>
-Please select a strategy and press Play.
+<?php
+if ( $human == -1 ) {
+    print "Please select a strategy and press Play.\n";
+} else if ( $human == 3 ) {
+    for($c=0;$c<3;$c++) {
+        for($h=0;$h<3;$h++) {
+            $r = check($c, $h);
+            print "Human=$names[$h] Computer=$names[$c] Result=$r\n";
+        }
+    }
+} else {
+    print "Your Play=$names[$human] Computer Play=$names[$computer] Result=$result\n";
+}
+?>
 </pre>
 </div>
-
-<script>
-function myFunction() {
-  var person = prompt("Please enter your name", "");
-  if (person != null) {
-    document.getElementById("demo").innerHTML =
-    "Welcome. " + person;
-  }
-
-   if ( person == "" ) {
-    alert("Name parameter missing.");
-    return false;
-    }
-}
-</script>
 </body>
 </html>
